01e0f272e777cd93967bb98fcc4fc19665cdeeb4
ayberkdogn <ayberkdogn@gmail.com>
2022-02-19 05:05:51 +0300

    first commit

---
+++ hw2.js
@@ -0,0 +1,24 @@
+function newElement() {
+    var liDOM = document.createElement('li')
+    var liADDED =document.getElementById("list").appendChild(liDOM)
+    var inputDOM=document.getElementById("task").value
+    liDOM.innerHTML=inputDOM
+    inputDOM.innerHTML=""
+
+    
+
+    
+
+       
+    //console.log(a)- ile test etmek istersek diye comment içinde verildi.
+
+    
+   
+    
+    
+
+
+
+
+
+}
